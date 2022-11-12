1a0f8e56bf2c41846fe20c270b0b1c8aedf41123
Amir Hakim <amirhakimadilayahya@gmail.com>
2022-11-12 21:09:45 +0700

    🏠 update

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <Layout>
       {/* Start Hero */}
-      <section className="w-full pt-36 pb-80 bg-[url('/family.jpg')] bg-cover">
+      <section className="w-full pt-36 pb-10 bg-[url('/family.jpg')] bg-cover">
         <div className="container">
           <div className="w-full items-center lg:w-1/2">
             <h1 className="font-header text-6xl text-white font-bold mt-12">
@@ -20,61 +20,83 @@ export default function Home() {
           </div>
           <div className="mt-5 mb-5">
             <div>
-              <div className="text-white text-2xl">Daftar Ansuransi</div>
+              <div className="text-white text-2xl border-b-4 w-44">Daftar Asuransi</div>
             </div>
           </div>
           <div className="flex flex-row">
             <div>
               <Link href={"/"}>
-                <div className="w-36 h-36 relative py-3 bg-white rounded-lg shadow-lg">
+                <div className="w-36 h-36 relative py-3 bg-white rounded-lg shadow-lg hover:bg-orange-400 hover:text-white">
                   <div className="flex flex-col justify-around items-center pt-5">
-                    <Image src={"/health.svg"} width={50} height={100} />
+                    <Image
+                      src={"/health.svg"}
+                      width={50}
+                      height={100}
+                      alt="icon"
+                    />
                     <h4>Asuransi Jiwa</h4>
-                    <h4>Bebas Rencana</h4>
+                    <h4 className="font-bold">Bebas Rencana</h4>
                   </div>
                 </div>
               </Link>
             </div>
             <div className="ml-4">
               <Link href={"/"}>
-                <div className="w-36 h-36 relative py-3 bg-white rounded-lg shadow-lg">
-                  <div className="flex flex-col justify-around items-center pt-5">
-                    <Image src={"/card.svg"} width={50} height={100} />
+                <div className="w-36 h-36 relative py-3 bg-white rounded-lg shadow-lg hover:bg-orange-400 hover:text-white">
+                  <div className="flex flex-col justify-around text-center items-center pt-2">
+                    <Image
+                      src={"/card.svg"}
+                      width={50}
+                      height={100}
+                      alt="icon"
+                    />
                     <h4>Asuransi Kesehatan</h4>
-                    <h4>Bebas Rencana</h4>
+                    <h4 className="font-bold">Asuransi Bebas Handal</h4>
                   </div>
                 </div>
               </Link>
             </div>
             <div className="ml-4">
               <Link href={"/"}>
-                <div className="w-36 h-36 relative py-3 bg-white rounded-lg shadow-lg">
+                <div className="w-36 h-36 relative py-3 bg-white rounded-lg shadow-lg hover:bg-orange-400 hover:text-white">
                   <div className="flex flex-col justify-around items-center pt-5">
-                    <Image src={"/chair.svg"} width={50} height={100} />
+                    <Image
+                      src={"/chair.svg"}
+                      width={50}
+                      height={100}
+                      alt="icon"
+                    />
                     <h4>Asuransi Kesehatan</h4>
-                    <h4>Bebas Rencana</h4>
+                    <h4 className="font-bold">Bebas Rencana</h4>
                   </div>
                 </div>
               </Link>
             </div>
             <div className="ml-4">
               <Link href={"/"}>
-                <div className="w-36 h-36 relative py-3 bg-white rounded-lg shadow-lg">
+                <div className="w-36 h-36 relative py-3 bg-white rounded-lg shadow-lg hover:bg-orange-400 hover:text-white">
                   <div className="flex flex-col justify-around items-center pt-5">
-                    <Image src={"/note.svg"} width={50} height={100} />
+                    <Image
+                      src={"/note.svg"}
+                      width={50}
+                      height={100}
+                      alt="icon"
+                    />
                     <h4>Asuransi Jiwa</h4>
-                    <h4>Bebas Rencana</h4>
+                    <h4 className="font-bold">Bebas Rencana</h4>
                   </div>
                 </div>
               </Link>
             </div>
           </div>
           <div className="mt-32">
-            <div className="text-white w-60 h-16 bg-orange-400 rounded-lg">
-              <div className="items-center justify-around pl-7 pt-5">
-                <h4>Temukan Produk Lainnya</h4>
+            <Link href={"/"}>
+              <div className="text-white w-60 h-16 bg-orange-400 rounded-lg">
+                <div className="items-center justify-around pl-7 pt-5">
+                  <h4>Temukan Produk Lainnya</h4>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
         </div>
       </section>
