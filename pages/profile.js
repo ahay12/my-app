177e099dba0a31203c8722bf021c231858e5ebf3
Amir Hakim <amirhakimadilayahya@gmail.com>
2022-12-01 20:30:44 +0700

    tambah gambar

--- pages/profile.js
+++ pages/profile.js
@@ -10,13 +10,33 @@ export default function About() {
             pic={"/amir.jpg"}
             f_nama={"AMIR HAKIM ADILA YAHYA"}
             f_nim={"17210253"}
-            b_title={"Slicing UI Design"}
+            b_title={"Frontend Developer"}
           />
 
-          <CardProfile f_nama={"MUHAMMAD LAZUARDI AGHANI"} f_nim={"17211079"} />
-          <CardProfile f_nama={"SALSABILATUL MA'WA"} f_nim={"17210903"} />
-          <CardProfile f_nama={"SITI FARIDA SYAHDA"} f_nim={"17210321"} />
-          <CardProfile f_nama={"RISMA LINDA"} f_nim={"17210987"} />
+          <CardProfile
+            pic={"/agan.jpeg"}
+            f_nama={"MUHAMMAD LAZUARDI AGHANI"}
+            f_nim={"17211079"}
+            b_title={"Frontend Developer"}
+          />
+          <CardProfile
+            pic={"/chaca.jpeg"}
+            f_nama={"SALSABILATUL MA'WA"}
+            f_nim={"17210903"}
+            b_title={"UI/UX Desain"}
+          />
+          <CardProfile
+            pic={"/rida.jpeg"}
+            f_nama={"SITI FARIDA SYAHDA"}
+            f_nim={"17210321"}
+            b_title={"UI/UX Desain"}
+          />
+          <CardProfile
+            pic={"/risma.jpeg"}
+            f_nama={"RISMA LINDA"}
+            f_nim={"17210987"}
+            b_title={"Testing"}
+          />
           <CardProfile f_nama={"IMPOSTOR"} f_nim={"??????"} />
         </Layout>
       </div>
